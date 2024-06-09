7112256a1f4d4b6eb190a9dccd1a9fdf8dc56b00
Elena Suchorukova <suhorukova.el@gmail.com>
2024-06-09 10:47:38 +0400

    Add slider

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+    speed: 1200,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/array-left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../icons/array-right.svg"></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          arrows: false,
+          dots: true
+        }
+      }
+    ]
+  });
+});
