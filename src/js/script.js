d7a9238242febc82821773717ad47eb5ecc04898
Elena Suchorukova <suhorukova.el@gmail.com>
2024-06-10 09:02:28 +0400

    Add catalog block

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('.carousel__inner').slick({
     speed: 1200,
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 2000,
     prevArrow: '<button type="button" class="slick-prev"><img src="../icons/array-left.svg"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="../icons/array-right.svg"></button>',
@@ -15,4 +15,24 @@ $(document).ready(function(){
       }
     ]
   });
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active')
+      .eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  function toggleSlide(className) {
+    $(className).each(function(i) {
+      $(this).on('click', function(e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      });
+    });
+  }
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
 });
